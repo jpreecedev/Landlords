add511fcf66fa07fc0c05d0d03706e300bb8dec6
Jon Preece <jonpreece@hotmail.co.uk>
2017-05-07 17:12:44 +0100

    Update packages

--- wwwroot/src/main.js
+++ wwwroot/src/main.js
@@ -1,27 +1,24 @@
-require('bootstrap')
-
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import router from './router'
 import store from './store'
-
 import './assets/styles/app.scss'
 import App from './components/App'
-
 import './extensions'
+import Auth from './auth'
+import Interceptors from './interceptors'
+import VeeValidate from 'vee-validate'
+
+require('bootstrap')
 
 Vue.config.productionTip = false
+
 Vue.use(VueResource)
-
-import Auth from './auth'
 Vue.use(Auth)
-
-import Interceptors from './interceptors'
 Vue.use(Interceptors)
-
-import VeeValidate from 'vee-validate'
 Vue.use(VeeValidate)
 
 new Vue({
