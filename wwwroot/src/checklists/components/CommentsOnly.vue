cfa0c676d1f5595939e00c90e45e9fb78693646f
Jon Preece <jonpreece@hotmail.co.uk>
2017-05-29 07:47:37 +0100

    Checlist template: Comments only (#27)

---
+++ wwwroot/src/checklists/components/CommentsOnly.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-block">
+    <div class="form-group row">
+      <div class="col">
+        <label class="col-form-label" v-bind:for="checklistItem.key + 'comments'">Comments</label>
+        <textarea v-model="checklistItem.comments" class="form-control" v-bind:id="checklistItem.key + 'comments'" placeholder="Enter any comments here" v-bind:name="checklistItem.key + 'comments'" type="text" rows="4"></textarea>                    
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comments-only',
+  props: {
+    'checklistItem': {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+  
+<style lang="scss" scoped>
+  
+</style>
