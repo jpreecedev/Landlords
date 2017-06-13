ec674263ff5c859d8756f2d9d9fd34a8ab7c9698
Jon Preece <jonpreece@hotmail.co.uk>
2017-06-13 18:56:18 +0100

    Navbar along top

--- wwwroot/src/root/components/AppNav.vue
+++ wwwroot/src/root/components/AppNav.vue
@@ -1,78 +1,78 @@
 <template>
-  <md-whiteframe class="whiteframe" md-elevation="2">
-    <nav>
-      <md-toolbar class="md-medium">
+  <v-toolbar light>
+    <v-toolbar-side-icon light></v-toolbar-side-icon>
+    <v-toolbar-title class="hidden-sm-and-down">Landlords</v-toolbar-title>
 
-        <md-button class="md-icon-button">
-          <md-icon>menu</md-icon>
-        </md-button>
+    <v-toolbar-items>
+      <v-toolbar-item v-if="auth.isLoggedIn" to="/dashboard">
+        Dashboard
+      </v-toolbar-item>
+      <v-toolbar-item v-if="permissions.LL_List" to="/agency/landlord-list">
+        Landlord List
+      </v-toolbar-item>
+      <v-toolbar-item v-if="permissions.PD_GetList" to="/manager/property-list">
+        Property List
+      </v-toolbar-item>
+      <v-toolbar-item v-if="permissions.PE_List" to="/permissions">
+        Permissions
+      </v-toolbar-item>
+      <v-toolbar-item v-if="permissions.CL_Overview" to="/checklists/">
+        Checklists
+      </v-toolbar-item>
+      <v-toolbar-item v-if="permissions.TE_GetListById" to="/tenants/">
+        Tenants
+      </v-toolbar-item>
+      <v-toolbar-item v-if="!auth.isLoggedIn" to="/registration/">
+        Log in or Register
+      </v-toolbar-item>
+      <v-toolbar-item v-if="permissions.AC_Overview" to="/accounts/">
+        Accounts
+      </v-toolbar-item>
 
-        <h2 class="md-title">Landlords</h2>
-        <router-link tag="button" class="md-button" v-if="auth.isLoggedIn" to="/dashboard">Dashboard</router-link>
-        <router-link v-if="permissions.LL_List" tag="button" class="md-button" to="/agency/landlord-list">
-          Landlord List
-        </router-link>
-        <router-link v-if="permissions.PD_GetList" tag="button" class="md-button" to="/manager/property-list">
-          Property List
-        </router-link>
-        <router-link v-if="permissions.PE_List" tag="button" class="md-button" to="/permissions">
-          Permissions
-        </router-link>
-        <router-link v-if="permissions.CL_Overview" tag="button" class="md-button" to="/checklists/">
-          Checklists
-        </router-link>
-        <router-link v-if="permissions.TE_GetListById" tag="button" class="md-button" to="/tenants/">
-          Tenants
-        </router-link>
-        <router-link v-if="!auth.isLoggedIn" tag="button" class="md-button" to="/registration/">
-          Log in or Register
-        </router-link>
-
-        <md-menu md-size="6" md-direction="bottom left">
-          <md-button md-menu-trigger>Calculators</md-button>
-
-          <md-menu-content>
-            <md-menu-item @click.native="$router.push('/calculators/rental-yield')">
+      <v-menu top left offset-y origin="bottom right" transition="v-slide-y-transition">
+        <v-toolbar-item slot="activator">
+          Calculators
+        </v-toolbar-item>
+        <v-list>
+          <v-list-item>
+            <v-list-tile @click.native="$router.push('/calculators/rental-yield')">
               Rental Yield
-            </md-menu-item>
-            <md-menu-item @click.native="$router.push('/calculators/monthly-payment')">
+            </v-list-tile>
+          </v-list-item>
+          <v-list-item>
+            <v-list-tile @click.native="$router.push('/calculators/monthly-payment')">
               Monthly Payment
-            </md-menu-item>
-            <md-menu-item @click.native="$router.push('/calculators/how-much-can-i-borrow')">
+            </v-list-tile>
+          </v-list-item>
+          <v-list-item>
+            <v-list-tile @click.native="$router.push('/calculators/how-much-can-i-borrow')">
               How much can I borrow?
-            </md-menu-item>
-            <md-menu-item @click.native="$router.push('/calculators/is-this-property-a-good-investment')">
+            </v-list-tile>
+          </v-list-item>
+          <v-list-item>
+            <v-list-tile @click.native="$router.push('/calculators/is-this-property-a-good-investment')">
               Is this property a good investment?
-            </md-menu-item>
-          </md-menu-content>
-        </md-menu>
+            </v-list-tile>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-        <router-link v-if="permissions.AC_Overview" tag="button" class="md-button" to="/accounts/">
-          Accounts
-        </router-link>
+      <v-menu top left offset-y origin="bottom right" transition="v-slide-y-transition">
+        <v-btn light icon slot="activator">
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-item v-if="auth.isLoggedIn" @click.native="logout()">
+            Log out
+          </v-list-item>
+          <v-list-item v-if="permissions.P_View" @click.native="$router.push('/profile')">
+            View profile
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
 
-        <md-menu md-size="3" v-if="auth.isLoggedIn" md-direction="bottom left">
-          <md-button md-menu-trigger class="md-icon-button">
-            <md-icon>more_vert</md-icon>
-          </md-button>
-
-          <md-menu-content>
-            <md-menu-item v-if="auth.isLoggedIn" @click.native="logout()">
-              Log out
-            </md-menu-item>
-            <md-menu-item v-if="permissions.P_View" @click.native="$router.push('/profile')">
-              View profile
-            </md-menu-item>
-          </md-menu-content>
-        </md-menu>
-
-        <md-avatar v-if="auth.isLoggedIn">
-          <img src="../../assets/images/avatar.jpg" alt="Avatar">
-        </md-avatar>
-
-      </md-toolbar>
-    </nav>
-  </md-whiteframe>
+  </v-toolbar>
 </template>
 
 <script>
