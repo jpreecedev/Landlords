6dd79654b1609b99099a28183d290b9c253473e3
Jon Preece <jonpreece@hotmail.co.uk>
2017-07-01 09:23:19 +0100

    Vuetify monthly payment calculator

--- wwwroot/src/root/components/calculators/MonthlyPayment.vue
+++ wwwroot/src/root/components/calculators/MonthlyPayment.vue
@@ -7,33 +7,37 @@
         <form role="form" novalidate>
           <div class="row">
             <div class="col-xs-12">
-              <md-input-container :class="{ 'md-input-invalid': errorBag.has('mortgageAmount') }">
-                <label for="mortgageAmount">Total amount borrowed</label>
-                <md-input type="number" id="mortgageAmount" min="0" name="mortgageAmount" v-model="mortgageAmount" data-vv-name="mortgageAmount" v-validate="'required|min_value:10000'" data-vv-validate-on="change" required />
-                <span v-if="errorBag.has('mortgageAmount:required')" class="md-error">Enter the amount borrowed</span>
-                <span v-else-if="errorBag.has('mortgageAmount:min_value')" class="md-error">Enter at least &pound;10,000</span>
-              </md-input-container>
+              <v-text-field v-model="mortgageAmount"
+                            :rules="[$validation.rules.required, $validation.rules.min_value(mortgageAmount, 10000)]"
+                            label="Total amount borrowed"
+                            type="number"
+                            required>
+              </v-text-field>
             </div>
           </div>
           <div class="row">
             <div class="col-xs-12 col-md-6">
-              <md-input-container :class="{ 'md-input-invalid': errorBag.has('annualInterestRate') }">
-                <label for="annualInterestRate">Interest rate % (annual)</label>
-                <md-input type="number" id="annualInterestRate" name="annualInterestRate" min="0.1" step="0.1" v-model="annualInterestRate" data-vv-name="annualInterestRate" v-validate="'required|min_value:0.1'" data-vv-validate-on="change" required />
-                <span v-if="errorBag.has('annualInterestRate:required')" class="md-error">Enter the amount borrowed</span>
-                <span v-else-if="errorBag.has('annualInterestRate:min_value')" class="md-error">Enter at least 0.1%</span>
-              </md-input-container>
+              <v-text-field v-model="annualInterestRate"
+                            :rules="[$validation.rules.required, $validation.rules.min_value(annualInterestRate, 0.1)]"
+                            label="Interest rate % (Annual)"
+                            type="number"
+                            min="0.1"
+                            step="0.1"
+                            required>
+              </v-text-field>
             </div>
           </div>
           <div class="row">
             <div class="col-xs-12 col-md-6">
-              <md-input-container :class="{ 'md-input-invalid': errorBag.has('mortgageLength') }">
-                <label for="mortgageLength">Mortgage length</label>
-                <md-input type="number" id="mortgageLength" name="mortgageLength" step="1" min="1" max="50" v-model="mortgageLength" data-vv-name="mortgageLength" v-validate="'required|min_value:1|max_value:50'" data-vv-validate-on="change" required />
-                <span v-if="errorBag.has('mortgageLength:required')" class="md-error">Enter the mortgage length</span>
-                <span v-else-if="errorBag.has('mortgageLength:min_value')" class="md-error">Enter at least 1 year</span>
-                <span v-else-if="errorBag.has('mortgageLength:max_value')" class="md-error">Enter up to 50 years</span>
-              </md-input-container>
+              <v-text-field v-model="mortgageLength"
+                            :rules="[$validation.rules.required, $validation.rules.min_value(mortgageLength, 1), $validation.rules.max_value(mortgageLength, 50)]"
+                            label="Mortgage length"
+                            type="number"
+                            min="1"
+                            step="1"
+                            max="50"
+                            required>
+              </v-text-field>
             </div>
           </div>
           <div class="row mt-4" v-if="!calculateMonthlyPayment">
