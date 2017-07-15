ede560a68f8128e4386f4ca2540184d554d8cc98
Jon Preece <jonpreece@hotmail.co.uk>
2017-07-15 07:34:58 +0100

    Only show back button when not on first step

--- wwwroot/src/journeys/StartTenancy.vue
+++ wwwroot/src/journeys/StartTenancy.vue
@@ -34,7 +34,7 @@
 
     <div class="row">
       <div class="col-xs-6">
-        <v-btn flat @click.native="back()">Go back</v-btn>
+        <v-btn flat @click.native="back()" v-if="newTenancy.step !== 1">Go back</v-btn>
       </div>
       <div class="col-xs-6 text-right">
         <v-btn primary @click.native="next()">{{ newTenancy.step === 5 ? 'Finished' : 'Continue' }}</v-btn>
